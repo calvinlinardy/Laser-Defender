ec791fdfdc3c73328a715f469b49e4e48c2b456e
calvinlinardy <calvin.linardy@yahoo.com>
2020-08-05 22:33:27 +0800

    First Commit
    beginning.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 10f;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+        var newXPos = transform.position.x + deltaX;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+        var newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
