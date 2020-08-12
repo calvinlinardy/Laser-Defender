ccc911c04c9b0970779df4855f65c8db02bcf4b2
calvinlinardy <calvin.linardy@yahoo.com>
2020-08-13 00:13:03 +0800

    Adding Scenes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
